f04320357a1a015ccbe9ee7a574e2ea333921834
lcydr <1211753823@qq.com>
2022-07-01 19:53:37 +0800

    完成案例品牌管理时间格式化

---
+++ src/1.vue过滤器.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{msg}}
+    {{msg | toUp}}
+    {{msg | toReverse}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'hello,vue'
+    }
+  },
+  filters:{
+    toUp(val){
+      return val.toUpperCase()
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
