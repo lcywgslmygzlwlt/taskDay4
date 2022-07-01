7237b64430020bebb759d31ab9c1153bf714e0ec
wgs66 <707205590@qq.com>
2022-07-01 20:46:06 +0800

    过滤器时间案例

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
