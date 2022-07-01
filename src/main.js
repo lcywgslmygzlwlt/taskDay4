f1c465423b01502f4de7b3ecd296d03364d7bc26
lcydr <1211753823@qq.com>
2022-07-01 19:46:14 +0800

    完成过滤器

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('toReverse', (val, split) => {
+  return val.split('').reverse().join(split || '')
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
