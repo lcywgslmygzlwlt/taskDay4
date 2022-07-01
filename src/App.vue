0f44eed9a5be4203ee92bdd3c7ad08e7b93e4ac8
lcydr <1211753823@qq.com>
2022-07-01 21:00:30 +0800

    完成附加练习_5.选你爱我求和

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,36 @@
 <template>
   <div>
-    <p>请选择你要购买的书籍</p>
-    <ul>
-      <li v-for="(item,index) in arr" :key="index">
-      {{item.name}}
-      <button @click.prevent="buy(item.name)">买书</button>
-      </li>
-    </ul>
-    <table border="1" width="500" cellspacing="0">
-      <tr>
-        <th>序号</th>
-        <th>书名</th>
-        <th>单价</th>
-        <th>数量</th>
-        <th>合计</th>
-      </tr>
-        <tr v-for="(item,index) in arr" :key="index">
-        <th>{{index+1}}</th>
-        <th>{{item.name}}</th>
-        <th>{{item.price}}</th>
-        <th>{{item.count}}</th>
-        <th>{{item.count * item.price}}</th>
-      </tr>
-    </table>
-    <p>总价格为:{{allPrice}} </p>
+        <!-- 无id时, 可以使用index(反正也是就地更新) -->
+    <div
+      v-for="(item, index) in arr"
+      style="display: inline-block"
+      :key="index"
+    >
+      <input type="checkbox" v-model="checkNumArr" :value="item" />
+      <span>{{ item }}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{ theSum }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      arr: [
-        {
-          name: "水浒传",
-          price: 107,
-          count: 0,
-        },
-        {
-          name: "西游记",
-          price: 192,
-          count: 0,
-        },
-        {
-          name: "三国演义",
-          price: 219,
-          count: 0,
-        },
-        {
-          name: "红楼梦",
-          price: 178,
-          count: 0,
-        },
-      ],
-    };
-  },
-  methods:{
-    buy(val){
-     const index= this.arr.findIndex(obj=>obj.name==val)
-      this.arr[index].count++
+  data(){
+    return{
+      arr:[9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+       checkNumArr:[]
     }
   },
   computed:{
-    allPrice(){
-     return this.arr.reduce((sum,next)=>{
-        return sum + next.count * next.price
+    theSum(){
+     return this.checkNumArr.reduce((sum,next)=>{
+        return sum += next
       },0)
     }
   }
-};
+}
 </script>
+
+<style>
+
+</style>
