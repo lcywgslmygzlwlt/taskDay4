0c8df65dad5ab0069b6c26fe3d4c36142273d06f
lcydr <1211753823@qq.com>
2022-07-02 15:52:39 +0800

    过滤器

---
+++ src/11.过滤器.vue
@@ -0,0 +1,41 @@
+<template>
+     
+    <div>
+         
+      <h2>国内网站：粉丝数量：{{ num | change("home") }}</h2>
+         
+      <h2>外内网站：粉丝数量：{{ num | change("abroad") }}</h2>
+         
+      <h3>时间：{{ time1 | beijingTime("-") }}</h3>
+         
+      <h3>时间：{{ time1 | beijingTime("/") }}</h3>
+       
+    </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      num: 1876986,
+      time1: 1636450540055,
+    };
+  },
+  filters: {
+    change(val, arg1) {
+      if (arg1 == "home") {
+        return val / 10000 + "万";
+      } else {
+        return val / 1000 + "千";
+      }
+    },
+    beijingTime(val, arg1) {
+      const date = new Date(val);
+      return moment(date).format("YYYY-MM-DD").replace(/\D/g, arg1);
+    },
+  },
+};
+</script>
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,133 +1,35 @@
 <template>
-  <div id="app">
-    <table class="tb">
-      <tr>
-        <th><input type="checkbox" v-model="isAll"  />全选</th>
-        <th>商品</th>
-        <th>单价</th>
-        <th>数量</th>
-        <th>小记</th>
-        <th>操作</th>
-      </tr>
-      <!-- 循环渲染的元素tr -->
-      <tr v-for="(item) in list" :key="item.id">
-        <td><input type="checkbox" v-model="item.a" />{{item.id}}</td>
-        <td>{{item.name}}</td>
-        <td>{{item.price}}</td>
-        <td><button @click="item.num <=0 ? item.num =0 : item.num-=1">-</button>
-        <input style="text-align:center" type="text" v-model="item.num"/>
-        <button @click="item.num+=1">+</button></td>
-        <td>{{item.price * item.num}}</td>
-        <td><button @click="del(item.id)">删除</button></td>
-      </tr>
-
-      <tr v-if="list.length === 0">
-        <td colspan="4">没有数据咯~</td>
-      </tr>
-    </table>
-    <br />
-    <button @click="btnDele">删除选中商品</button>
-    <button @click="empty">清理购物车</button>
-    <br />
-    <div style="margin-top: 20px">
-      <h2>统计</h2>
-      <p>已经选中商品件数: {{ sumCommodity }}</p>
-      <p>总价:{{sumPrice}}</p>
+    <div>
+      <h2>国内网站：粉丝数量：{{ num | change("home") }}</h2>
+      <h2>外内网站：粉丝数量：{{ num | change("abroad") }}</h2>
+      <h3>时间：{{ time1 | time2("-") }}</h3>
+      <h3>时间：{{ time1 | time2("/") }}</h3>
     </div>
-  </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   data() {
     return {
-      list: [
-        { id: 1, name: "奔驰", time: "2020-08-01",price:100,a:false,num:0 },
-        { id: 2, name: "宝马", time: "2020-08-02",price:200,a:false,num:0 },
-        { id: 3, name: "奥迪", time: "2020-08-03",price:300,a:false,num:0 },
-      ],
-      // choice:"",
-      // sumCommodity:0
+      num: 1876986,
+      time1: 1636450540055,
     };
   },
-  methods: {
-    del(id) {
-      // 删除按钮 - 得到索引, 删除数组里元素
-     const index = this.list.forEach(obj =>obj.id == id);
-      this.list.splice(index, 1);
+  filters: {
+    change(val, arg1) {
+      if (arg1 == "home") {
+        return val / 10000 + "万";
+      } else {
+        return val / 1000 + "千";
+      }
     },
-    // 删除选中商品
-    btnDele(){
-      this.list = this.list.filter((item)=>{
-        return !item.a
-      })
+    time2(val, arg1) {
+      const date = new Date(val);
+      return moment(date).format("YYYY-MM-DD").replace(/\D/g, arg1);
     },
-  // 清理购物车
-    empty(){
-      this.list = this.list.filter((item)=>{
-        return item.id>999
-      })
-    },
-    // sumCommodity(){ 
-    //   return this.list.filter(item => item.a == true).reduce((t,item)=>(t+=item),0)
-    // }
   },
-  computed:{
-    isAll:{
-      set(val){
-        this.list.forEach(obj => obj.a = val);
-      },
-      get(){
-        return this.list.length === 0 ? false : this.list.every((ele)=>(ele.a == true))
-      //  return this.list.every((ele)=>(ele.a == true))
-      }
-      }, 
-      sumCommodity:{
-        get(){
-           return this.list.filter(item => item.a == true).reduce((t,item)=>(t+=item.num),0)
-        }
-      },
-      sumPrice:{
-        get(){
-          return this.list.filter(item => item.a == true).reduce((total,item) => total+=item.price*item.num,0)
-        }
-      }
-      // add(index){
-      //   this.list[index].num++
-      // },
-      // sub(){
-      //   this.list[index].num--
-      // }
-    }
 };
 </script>
-
 <style>
-#app {
-  width: 600px;
-  margin: 10px auto;
-}
-
-.tb {
-  border-collapse: collapse;
-  width: 100%;
-}
-
-.tb th {
-  background-color: #0094ff;
-  color: white;
-}
-
-.tb td,
-.tb th {
-  padding: 5px;
-  border: 1px solid black;
-  text-align: center;
-}
-
-.add {
-  padding: 5px;
-  border: 1px solid black;
-  margin-bottom: 10px;
-}
 </style>
